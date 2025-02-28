5981186409da703e820e2e10550a20c50a6aeb78
Taha Hussain <thussain170305@gmail.com>
2025-02-28 16:35:02 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+// QUESTION 1..
+
+// Write a program that
+
+// a. Store correct password in a JS variable.
+// b. Asks user to enter his/her password
+// c. Validate the two passwords:
+// i. Check if user has entered password. If not, then
+// give message “ Please enter your password”
+// ii. Check if both passwords are same. If they are
+// same, show message “Correct! The password you
+// entered matches the original password”. Show
+// “Incorrect password” otherwise.
+
+
+// let a = prompt("Enter your password")
+// let pass = "pakistan"
+
+// if ( a === "") {
+//     alert("Please enter a password")
+// } else if ( a === pass) {
+//     alert("Correct password")
+// } else {
+//     alert("Incorrect password !")
+// }
+
+
+// This if/else statement does not work. Try to fix it:
+// var greeting;
+// var hour = 13;
+// if (hour < 18) {
+// greeting = "Good day";
+// else
+// greeting = "Good evening";
+
+// let a = +prompt("Enter a hour b/w 13 to 18")
+// let hour = 13
+
+// if ( a <= 18) {
+//     console.log("Good Day")
+// } else {
+//     console.log("Good Evening");
+    
+// }
+
+
+// Write a program that takes input a number from user &
+// state whether the number is positive, negative or zero.
+
+
+// let a = +prompt("Enter number")
+
+// if ( a > 0 ) {
+//     console.log("Number is positive");
+    
+// } else if ( a < 0) {
+//     console.log("Number is negative");
+// } else {
+//     console.log("number is 0")
+// }
+
+
+// Write a program that takes a character (number or string)
+// in a variable & checks whether the given input is a
+// number, uppercase letter or lower case letter. (Hint: ASCII
+// codes:- A=65, Z=90, a=97, z=122).
+
+
+// let a = +prompt("Enter a number or string")
+
+
+// if ( a >= 48 && a <= 57) {
+//     console.log("The input is a number")
+// } else if ( a >= 65 && a <= 90) {
+//     console.log("The input is an uppercase letter");
+    
+// } else if ( a >= 97 && a <= 122) {
+//     console.log("The input is a lowercase letter");
+    
+// } else {
+//     console.log("The number is a special character");
+    
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
